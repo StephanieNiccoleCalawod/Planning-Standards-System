a0c82021498780c24a9c2043e0ee4c8216e3a3ae
StephanieNiccoleCalawod <stephanieniccolecalawod@gmail.com>
2026-05-31 01:24:17 +0800

    fixed docker

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       // Service Catalogue service runs on port 3000
       '/api/services': {
-        target: 'http://127.0.0.1:3000',
+        target: process.env.PROXY_CATALOGUE_URL || 'http://127.0.0.1:3000',
         changeOrigin: true,
       },
       // All other API endpoints go to KPI/SLA standards service on port 3001
       '/api': {
-        target: 'http://127.0.0.1:3001',
+        target: process.env.PROXY_KPI_SLA_URL || 'http://127.0.0.1:3001',
         changeOrigin: true,
       }
     }
