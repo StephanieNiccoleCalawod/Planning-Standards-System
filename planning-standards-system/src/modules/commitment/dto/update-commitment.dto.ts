66e66e483b694ed56cc37e760451007e4e67f14d
paula Villacarlos <paulamichellenvillacarlos@domain.com>
2026-06-07 05:10:17 +0800

    chore: isolate service-catalogue feature by removing other services and pages

--- planning-standards-system/src/modules/commitment/dto/update-commitment.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCommitmentDto, CreateCommitmentItemDto } from './create-commitment.dto';
-
-export class UpdateCommitmentDto extends PartialType(CreateCommitmentDto) {
-  items?: CreateCommitmentItemDto[];
-}
--- planning-standards-system/src/modules/kpi-sla/dto/update-sla-rule.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSlaRuleDto } from './create-sla-rule.dto';
-
-export class UpdateSlaRuleDto extends PartialType(CreateSlaRuleDto) {}
